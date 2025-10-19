dd881d13343ca3374486a741f545a990a766e6e9
Sherif Ashraf <sherifashraf910@gmail.com>
2025-10-19 16:14:30 +0300

    add mock user info

---
+++ lib/mock/mock_user.dart
@@ -0,0 +1,26 @@
+import '../models/user_info.dart';
+
+// final user = UserInfo(
+//   name: "Sherif Ashraf",
+//   email: "sherif@sherif.com",
+//   phoneNumber: "+201062232138",
+//   dateOfBirth: "1994-02-04T00:00:00Z",
+//   gender: "male",
+//   nationality: "Egyptian",
+//   address: "Egypt, Zagazig",
+//   bio: "Mobile Developer specialized, passionate about building smooth, high-quality apps with clean architecture.",
+//   profileImageUrl: "https://i.postimg.cc/ZRpyxjBY/s.png",
+// );
+
+UserInfo get userInfo => UserInfo(
+  name: "Sherif Ashraf",
+  email: "sherif@sherif.com",
+  phoneNumber: "+201062232138",
+  dateOfBirth: "1994-02-04T00:00:00Z",
+  gender: "Male",
+  nationality: "Egyptian",
+  address: "Egypt, Zagazig",
+  bio:"Mobile developer skilled in SwiftUI and Flutter, "
+      "passionate about building high-quality apps with clean architecture and exploring new technologies.",
+  profileImageUrl: "https://i.postimg.cc/ZRpyxjBY/s.png",
+);
