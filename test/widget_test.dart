f647d9e5cb0594a920942f5ebd478b6c1805ac88
Sherif Ashraf <sherifashraf910@gmail.com>
2025-10-19 16:35:30 +0300

    rename main to profile

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:profile_demo_app_with_flutter/main.dart';
+import 'package:profile_demo_app_with_flutter/profile.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
