bf2c8a41b3e9d70e7c02f4ac68476cc7a7133391
Sherif Ashraf <sherifashraf910@gmail.com>
2025-10-19 16:13:21 +0300

    add user info model

---
+++ lib/models/user_info.dart
@@ -0,0 +1,25 @@
+
+class UserInfo {
+  final String name;
+  final String email;
+  final String phoneNumber;
+  final String dateOfBirth;
+  final String gender;
+  final String nationality;
+  final String address;
+  final String bio;
+  final String profileImageUrl;
+
+  UserInfo({
+    required this.name,
+    required this.email,
+    required this.phoneNumber,
+    required this.dateOfBirth,
+    required this.gender,
+    required this.nationality,
+    required this.address,
+    required this.bio,
+    required this.profileImageUrl
+  });
+}
+
