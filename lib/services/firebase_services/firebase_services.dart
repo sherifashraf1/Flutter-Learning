35298f42ab7d81c944f4fa9a7d95a84a32bc7d2d
Sherif Ashraf <sherifashraf910@gmail.com>
2025-11-12 03:05:23 +0200

    add firebase services fro signIn and create account and forget password

---
+++ lib/services/firebase_services/firebase_services.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseServices {
+ static Future<void> signIn(String email, String password) async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+ static Future<void> createAccount(String email, String password) async {
+    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+ static Future<void> resetPassword(String email) async {
+   await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+ }
+
+ static Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+}
